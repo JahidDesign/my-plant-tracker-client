e3e91bbc852cb9b463cf27aac9b18a9bd709efa4
JahidDesign <jahidhasan290113@gmail.com>
2025-08-07 19:54:10 +0600

    up

--- src/pages/AddBlogPost.jsx
+++ src/pages/AddBlogPost.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { useForm } from "react-hook-form";
 import axios from "axios";
 import Swal from "sweetalert2";
-import { Helmet } from "react-helmet";
+import { Helmet } from "react-helmet-async";
 
 const AddBlogPost = () => {
   const { register, handleSubmit, reset } = useForm();
